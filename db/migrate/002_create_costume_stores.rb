beacdd7137ad5b64d0e722e513afba96a75b5d09
Sarah Schaller <sarcsch@aol.com>
2017-09-19 15:19:35 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,5 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
-  def change 
+  def change
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
@@ -7,7 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :employee_count
       t.boolean :still_in_business
       t.datetime :opening_time
-      t.datetime :closing_time 
+      t.datetime :closing_time
     end
   end
 end
